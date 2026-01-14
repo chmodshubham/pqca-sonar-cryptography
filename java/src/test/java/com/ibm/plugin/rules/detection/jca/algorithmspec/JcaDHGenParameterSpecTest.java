a5bdab4c9ea2577efc71ede9afc44abcbbbee5c9
Shubham Kumar <chmodshubham@gmail.com>
2026-01-15 00:58:26 +0530

    updated same dependencies across cbomkit
    Signed-off-by: Shubham Kumar <chmodshubham@gmail.com>

--- java/src/test/java/com/ibm/plugin/rules/detection/jca/algorithmspec/JcaDHGenParameterSpecTest.java
+++ java/src/test/java/com/ibm/plugin/rules/detection/jca/algorithmspec/JcaDHGenParameterSpecTest.java
@@ -16,7 +16,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
+ * */
 package com.ibm.plugin.rules.detection.jca.algorithmspec;
 
 import static org.assertj.core.api.Assertions.assertThat;
